72e653fafc1d69e6a1e62e6585ef16bd3050b1e0
julenia <julia.jankowska@onet.eu>
2019-07-10 11:27:27 +0200

    adds get yield in euros function to crop and relevant tests along

--- crops.js
+++ crops.js
@@ -2,23 +2,30 @@ class Crop {
     constructor(acres) {
       this.acres = acres
     }
+    getYieldInEuros(){
+        return this.getYieldInKg()*this.price;
+    }
+
   }
   
 
-class Wheat {
+class Wheat extends Crop {
     constructor(acres) {
-      this.acres = acres
-    }
+        super(acres)
+        this.price = 1.5
+      }
   
     getYieldInKg() {
       return Math.pow(this.acres * 1.5, 1.3)
     }
   }
 
-  class Sugarcane {
-      constructor(acres) {
-          this.acres = acres
+  class Sugarcane extends Crop {
+    constructor(acres) {
+        super(acres)
+        this.price = 2
       }
+    
       getYieldInKg() {
           return Math.pow(this.acres * 2.6, 1.1)
       }
--- crops.spec.js
+++ crops.spec.js
@@ -25,3 +25,18 @@ test('A crop can store its area size', () => {
     const crop = new Crop(12345)
     expect(crop.acres).toBe(12345)
   })
+
+  test('Wheat class calculates the proper yield value', () => {
+    const wheat = new Wheat(100)
+    expect(wheat.getYieldInEuros()).toBeCloseTo(1011.6, 1)
+  })
+  
+  test('Sugarcane class calculates the proper yield value', () => {
+    const sugarcane = new Sugarcane(200)
+    expect(sugarcane.getYieldInEuros()).toBeCloseTo(1943.7, 1)
+  })
+  
+  test('Sugarcane class calculates the proper yield value', () => {
+    const sugarcane = new Sugarcane(0)
+    expect(sugarcane.getYieldInEuros()).toBe(0)
+  })
